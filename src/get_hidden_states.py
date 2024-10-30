f72ca0bf15b8b5ff860b075596c8f4b2875eb0f1
gallifantjack <gallifantjack@gmail.com>
2024-10-30 12:27:20 +0000

    Update cache clearing between batches Runs v1 tasks extraction sucessfully

--- src/get_hidden_states.py
+++ src/get_hidden_states.py
@@ -7,20 +7,23 @@ from retrieve_sae_features import retrieve_sae_features  # Import the function h
 import logging
 from tqdm import tqdm
 import math
+import gc
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+
 def get_valid_token(model_name, model_type, hidden_states):
-    if model_type == 'vlm':
-        if model_name == 'Intel/llava-gemma-2b':
+    if model_type == "vlm":
+        if model_name == "Intel/llava-gemma-2b":
             return hidden_states[:, 4:580, :]
-        elif model_name == 'google/paligemma-3b-mix-224': 
+        elif model_name == "google/paligemma-3b-mix-224":
             return hidden_states[:, 0:256, :]
-    elif model_type == 'llm':
+    elif model_type == "llm":
         print(hidden_states.size())
         return hidden_states[:, :-1, :]
     else:
-        logging.exception(f"wrong model, not support") 
+        logging.exception(f"wrong model, not support")
+
 
 def get_hidden_states(
     model_name,
@@ -31,26 +34,30 @@ def get_hidden_states(
     batch_size=32,
     dataset_name=None,
     dataset_config_name=None,
-    dataset_split='train',
+    dataset_split="train",
     text_field=None,
     image_field=None,
-    label_field='label',
+    label_field="label",
     max_batches=None,
     save_dir="./npz_files",  # Add save_dir to pass to retrieve_sae_features
-    activation_only=True
+    activation_only=True,
 ):
     logging.info(f"Loading model and tokenizer/processor for {model_name}")
     # Load the model and tokenizer/processor
-    model, tokenizer_or_processor, sae = load_models_and_tokenizer(checkpoint, layer, device, model_type, sae_release)
+    model, tokenizer_or_processor, sae = load_models_and_tokenizer(
+        checkpoint, layer, device, model_type, sae_release
+    )
     model.eval()
 
     # Load the dataset
-    logging.info(f"Loading dataset: {dataset_name}, config: {dataset_config_name}, split: {dataset_split}")
+    logging.info(
+        f"Loading dataset: {dataset_name}, config: {dataset_config_name}, split: {dataset_split}"
+    )
     dataset = load_dataset(dataset_name, name=dataset_config_name, split=dataset_split)
 
     # Include unique IDs if available, else use indices
-    if 'id' not in dataset.column_names:
-        dataset = dataset.map(lambda example, idx: {'id': idx}, with_indices=True)
+    if "id" not in dataset.column_names:
+        dataset = dataset.map(lambda example, idx: {"id": idx}, with_indices=True)
 
     def custom_collate_fn(batch):
         batch_out = {}
@@ -59,7 +66,9 @@ def get_hidden_states(
             batch_out[key] = [sample[key] for sample in batch]
         return batch_out
 
-    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, collate_fn=custom_collate_fn)
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, collate_fn=custom_collate_fn
+    )
 
     # Calculate total number of batches
     total_samples = len(dataset)
@@ -68,23 +77,35 @@ def get_hidden_states(
         total_batches = max_batches
 
     with torch.no_grad():
-        for batch_idx, batch in enumerate(tqdm(data_loader, total=total_batches, desc="Processing Batches")):
+        for batch_idx, batch in enumerate(
+            tqdm(data_loader, total=total_batches, desc="Processing Batches")
+        ):
             if max_batches is not None and batch_idx >= max_batches:
-                logging.info(f"Reached maximum number of batches ({max_batches}). Stopping.")
+                logging.info(
+                    f"Reached maximum number of batches ({max_batches}). Stopping."
+                )
                 break
             try:
                 # Retrieve texts and images based on specified field names
                 if model_type == "vlm":
                     images = batch.get(image_field)
                     if images is None:
-                        raise ValueError(f"Image field '{image_field}' not found in the dataset.")
-                    texts = [''] * len(images)
-                    inputs = prepare_inputs(images, texts, tokenizer_or_processor, device, model_type)
+                        raise ValueError(
+                            f"Image field '{image_field}' not found in the dataset."
+                        )
+                    texts = [""] * len(images)
+                    inputs = prepare_inputs(
+                        images, texts, tokenizer_or_processor, device, model_type
+                    )
                 else:
                     texts = batch.get(text_field)
                     if texts is None:
-                        raise ValueError(f"Text field '{text_field}' not found in the dataset.")
-                    inputs = prepare_inputs(None, texts, tokenizer_or_processor, device, model_type)
+                        raise ValueError(
+                            f"Text field '{text_field}' not found in the dataset."
+                        )
+                    inputs = prepare_inputs(
+                        None, texts, tokenizer_or_processor, device, model_type
+                    )
 
                 # Extract hidden states for the current batch
                 hidden_states = extract_hidden_states(model, inputs, layer, model_type)
@@ -92,10 +113,24 @@ def get_hidden_states(
                 hidden_states = get_valid_token(model_name, model_type, hidden_states)
                 # print(type(hidden_states), hidden_states.size())
                 labels = batch.get(label_field)
-                ids = batch.get('id')  # Get the unique IDs
+                ids = batch.get("id")  # Get the unique IDs
 
                 # Process and save the hidden states and labels immediately
-                retrieve_sae_features(model_name, layer, hidden_states, labels, ids, sae, save_dir=save_dir, activation_only=activation_only)
+                retrieve_sae_features(
+                    model_name,
+                    layer,
+                    hidden_states,
+                    labels,
+                    ids,
+                    sae,
+                    save_dir=save_dir,
+                    activation_only=activation_only,
+                )
+                # After processing the batch clear the memory
+                del hidden_states, inputs
+                torch.cuda.empty_cache()
+                gc.collect()
+
             except Exception as e:
                 logging.exception(f"Error processing batch {batch_idx + 1}")
                 continue
--- src/run_ds.sh
+++ src/run_ds.sh
@@ -14,28 +14,89 @@ export CUDA_VISIBLE_DEVICES=0  # Use only GPU 0
 #     --act_only False \
 #     --max_batches 3
 
-# python run.py --model_name google/gemma-2b-it \
-#     --model_type llm \
-#     --sae_release gemma-2b \
-#     --layer 12 \
-#     --checkpoint google/gemma-2b-it \
-#     --save_dir ./output_llm_both \
-#     --dataset_name shanchen/OncQA \
-#     --dataset_split train \
-#     --text_field question \
-#     --image_field NA \
-#     --label_field q1 \
-#     --act_only False \
-#     --max_batches 3 
-
-python process_npz_files.py \
-    --input-dir ./output_llm_both \
-    --model-name google/gemma-2b-it \
-    --model-type llm \
+python run.py --model_name google/gemma-2b-it \
+    --model_type llm \
+    --sae_release gemma-2b \
     --layer 12 \
-    --sae-release gemma-2b \
-    --top-n 5 \
-    --output-dir processed_features_llm \
-    --test-size 0.2 \
-    --tree-depth 5 \
-    --save-plots
+    --checkpoint google/gemma-2b-it \
+    --save_dir ./output_llm_both/election/gemma_2b_it \
+    --dataset_name Anthropic/election_questions \
+    --dataset_split test \
+    --text_field question \
+    --batch_size 16 \
+    --image_field NA \
+    --label_field label \
+    --act_only False 
+    # --max_batches 3 
+
+python run.py --model_name google/gemma-2b-it \
+    --model_type llm \
+    --sae_release gemma-2b \
+    --layer 12 \
+    --checkpoint google/gemma-2b-it \
+    --save_dir ./output_llm_both/toxic_en/gemma_2b_it \
+    --dataset_name textdetox/multilingual_toxicity_dataset \
+    --dataset_split en \
+    --text_field text \
+    --batch_size 16 \
+    --image_field NA \
+    --label_field toxic \
+    --act_only False 
+    # --max_batches 3 
+
+python run.py --model_name google/gemma-2b-it \
+    --model_type llm \
+    --sae_release gemma-2b \
+    --layer 12 \
+    --checkpoint google/gemma-2b-it \
+    --save_dir ./output_llm_both/toxic_zh/gemma_2b_it \
+    --dataset_name textdetox/multilingual_toxicity_dataset \
+    --dataset_split zh \
+    --text_field text \
+    --batch_size 16 \
+    --image_field NA \
+    --label_field toxic \
+    --act_only False 
+    # --max_batches 3 
+
+python run.py --model_name google/gemma-2b-it \
+    --model_type llm \
+    --sae_release gemma-2b \
+    --layer 12 \
+    --checkpoint google/gemma-2b-it \
+    --save_dir ./output_llm_both/reject/gemma_2b_it \
+    --dataset_name AIM-Harvard/reject_prompts \
+    --dataset_split train \
+    --text_field text \
+    --batch_size 16 \
+    --image_field NA \
+    --label_field label \
+    --act_only False 
+    # --max_batches 3 
+
+python run.py --model_name google/gemma-2b-it \
+    --model_type llm \
+    --sae_release gemma-2b \
+    --layer 12 \
+    --checkpoint google/gemma-2b-it \
+    --save_dir ./output_llm_both/jailbreak/gemma_2b_it \
+    --dataset_name jackhhao/jailbreak-classification \
+    --dataset_split train \
+    --text_field prompt \
+    --batch_size 16 \
+    --image_field NA \
+    --label_field type \
+    --act_only False 
+    # --max_batches 3 
+
+# python process_npz_files.py \
+#     --input-dir ./output_llm_both \
+#     --model-name google/gemma-2b-it \
+#     --model-type llm \
+#     --layer 12 \
+#     --sae-release gemma-2b \
+#     --top-n 5 \
+#     --output-dir processed_features_llm \
+#     --test-size 0.2 \
+#     --tree-depth 5 \
+#     --save-plots
